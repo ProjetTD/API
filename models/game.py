3f9b7f8c1eb3baaf8bf71d6af86920ec03967fb2
Swixos <erazion.swixo@gmail.com>
2024-01-31 21:35:21 +0100

    Update CORS settings and add foreign key constraints

--- models/game.py
+++ models/game.py
@@ -7,8 +7,8 @@ engine = create_engine(sqlite_url, echo=True)
 
 class Game(SQLModel, table=True):
     id_game: int = Field(primary_key=True, index=True)
-    id_player: int
-    id_level: int
+    id_player: int = Field(foreign_key="player.id_player")
+    id_level: int = Field(foreign_key="level.id_level")
     score: int
     status: str
 
